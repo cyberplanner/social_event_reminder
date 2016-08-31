8be2cf588553bfff3a677d975cecc161c83569ae
cyberplanner <saleh.gfx@gmail.com>
2016-08-31 13:13:55 +0100

    Adds user signup, test passes.

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -5,10 +5,10 @@ feature 'Sign up' do
     visit '/'
     click_link 'Sign up'
     fill_in 'Email', with: "deadpool@marvel.com"
-    fill_in 'Password', with: "SuperSecret7"
-    fill_in 'Password confirmation', with: "SuperSecret7"
+    fill_in 'Password', with: "whatever", :match => :prefer_exact
+    fill_in 'Password confirmation', with: "whatever", :match => :prefer_exact
     click_button 'Sign up'
     expect(page.current_path).to eq '/'
-    expect(page).to have_content 'Welcome! you have signed up successfully.'
+    expect(page).to have_content 'Welcome! You have signed up successfully'
   end
 end
