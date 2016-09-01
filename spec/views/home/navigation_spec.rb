3fa394e7e046e70660b1b5647a61326010dd3576
cyberplanner <saleh.gfx@gmail.com>
2016-09-01 13:30:35 +0100

    Only signed in users can view events.

--- spec/views/home/navigation_spec.rb
+++ spec/views/home/navigation_spec.rb
@@ -6,4 +6,12 @@ feature 'Navbar' do
     click_link 'Home'
     expect(page.current_path).to eq '/'
   end
+
+  scenario "new users can't see event on homepage" do
+    sign_up
+    create_event
+    sign_out
+    click_link 'Home'
+    expect(page).not_to have_content 'Graduation'
+  end
 end
